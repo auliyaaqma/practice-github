db2a48ed1446ae519b1e5845c06838d287c1dd77
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2017-09-21 08:22:23 +0700

    yey coba lagi

---
+++ src/travlendar_a2/percobaan_lagi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travlendar_a2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class percobaan_lagi {
+  //ini percobaan, mau ngecek sinkron langsung apa engga  
+}
